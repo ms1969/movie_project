28118fda384b4a35ece6d984cbfa7f5a8a057227
ms1969 <maxmaxm@mail.ru>
2022-01-18 22:03:13 +0800

    Slug

--- movie_app/views.py
+++ movie_app/views.py
@@ -6,13 +6,15 @@ from .models import Movie
 
 def show_all_movie(request):
     movies = Movie.objects.all()
+    # for movie in movies:
+    #     movie.save()
     return render(request, 'movie_app/all_movies.html', {
         'movies': movies
     })
 
 
-def show_one_movie(request, id_movie:int):
-    movie = get_object_or_404(Movie, id=id_movie)
+def show_one_movie(request, slug_movie:str):
+    movie = get_object_or_404(Movie, slug=slug_movie)
     return render(request, 'movie_app/one_movie.html', {
         'movie': movie
     })
